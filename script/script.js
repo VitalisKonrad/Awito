83b4a9e28c71a0d50613a0168b36b6155588ceb6
VitalisKonrad <46555488+VitalisKonrad@users.noreply.github.com>
2020-07-14 22:46:03 +0300

    add modal window show/close

--- script/script.js
+++ script/script.js
@@ -1,5 +1,20 @@
-'use strict';
+// 'use strict';
 
-const modalAdd = document.querySelector('.modal__add');
+const modalAdd = document.querySelector('.modal__add'),
+    addAd = document.querySelector('.add__ad'),
+    modalBtnSubmit = document.querySelector('.modal__btn-submit'),
+    modalSubmit = document.querySelector('.modal__submit');
 
-console.log(modalAdd);
+addAd.addEventListener('click', () => {
+    modalAdd.classList.remove('hide'),
+    modalBtnSubmit.disabled = true;
+});
+
+modalAdd.addEventListener('click', event => {
+    const target = event.target;
+    if(target.classList.contains('modal__close') ||
+    target === modalAdd){
+        modalAdd.classList.add('hide')
+        modalSubmit.reset();
+    }
+});
